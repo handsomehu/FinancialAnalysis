e6116455dfebc2cd35048743561ea7ab77e70d5c
I038825 <I038825@PVGN34319777A.apj.global.corp.sap>
2019-09-10 15:07:12 +0800

    plot pal

--- FuturesAna/tradeana/palana.py
+++ FuturesAna/tradeana/palana.py
@@ -5,16 +5,28 @@ Created on Sep 9, 2019
 '''
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
 
 
 pal = pd.read_csv("../tradelog/tr.csv",index_col = 0)
 print(pal)
 # by order
-pal.plot(y = "pal", kind= 'bar')
+fig, ax = plt.subplots()
+pal["pal"].plot(ax=ax,kind = 'bar')
 plt.show()
+#sys.exit(0)
+#data.groupby(['date','type']).count()['amount'].unstack().plot(ax=ax)
 # by strategy
+fig, ax = plt.subplots()
+pal.groupby("strategyname").sum()['pal'].plot(ax=ax,kind='bar')
+ax.legend()
+plt.show()
 
-bystrategy = pal.groupby("strategyname")
-bs = bystrategy.sum()
-bs.plot(x = "strategyname",y = "pal", kind= 'bar')
+fig, ax = plt.subplots()
+pal.groupby("comdt").sum()['pal'].plot(ax=ax,kind='bar')
+plt.show()
+
+fig, ax = plt.subplots()
+pal.groupby(["comdt","strategyname"]).sum()['pal'].plot(ax=ax,kind='bar')
+fig.legend()
 plt.show()
