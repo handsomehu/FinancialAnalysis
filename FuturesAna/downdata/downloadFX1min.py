ac2022ea8a84c9c13c7cb29909da91d2decdb14d
handsomehu <leonzh18@hotmail.com>
2020-04-24 16:38:20 +0800

    update download

--- FuturesAna/downdata/downloadFX1min.py
+++ FuturesAna/downdata/downloadFX1min.py
@@ -10,9 +10,13 @@ Created on Mar 11, 2019
 '''
 import jqdatasdk as jqd
 import pandas as pd
+import os
 
-
-jqd.auth("18621861857", "")
+def readname():
+    filePath = "./data1m/"
+    name = os.listdir(filePath)
+    return name
+jqd.auth("18621861857", "x")
 
 #symbolName = "RB8888.XSGE"
 s0 = ["A8888.XDCE","AL8888.XSGE","AU8888.XSGE","B8888.XDCE","C8888.XDCE","CF8888.XZCE","CU8888.XSGE","ER8888.XZCE","FU8888.XSGE","GN8888.XZCE","IF8888.CCFX","J8888.XDCE","L8888.XDCE","M8888.XDCE","ME8888.XZCE","P8888.XDCE","PB8888.XSGE","RB8888.XSGE","RO8888.XZCE","RU8888.XSGE","SR8888.XZCE","TA8888.XZCE","V8888.XDCE","WR8888.XSGE","WS8888.XZCE","WT8888.XZCE","Y8888.XDCE","ZN8888.XSGE"]
@@ -27,12 +31,21 @@ slist = [
             ["CJ8888.XZCE","EG8888.XDCE","SC8888.XINE","SP8888.XSGE","TS8888.CCFX"]
     
     ]
-slist = [["TS8888.CCFX"]]
+#slist = [["EG8888.XDCE"]]
+
+dones = readname()
+print(dones)
 for sub in slist:
 
     for sb in sub:
-  
+        symbol = sb.split(".")[0]
+        print(symbol)
+
+        if symbol+"_1m.csv" in dones:
+            print("there")
+            continue
+
         DF = jqd.get_price(sb, start_date='2009-01-01', end_date='2020-04-21', frequency='1m',fq = 'pre',fields=['open','close','high','low','volume']) # 获得IC1506.CCFX的分钟数据, 只获取open+close字段
-        DF.to_csv("./data1m/"+sb+"_1m.csv")
+        DF.to_csv("./data1m/"+symbol+"_1m.csv")
     #DF.to_hdf('pn2_years.h5', 'keyyear')
     #DF = pd.read_hdf("pn1.h5",'key')
